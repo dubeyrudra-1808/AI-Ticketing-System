9e8eb33ac2c8f6594ab9d3dd66a94196819fdbe5
Rudra Dubey <dubeyrudra63+work@gmail.com>
2025-06-16 01:33:07 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyAermDiexIaqHMqS-FghSclJxUMDRbg8vE")
-
-models = genai.list_models()
-
-print("Available models for this API key:")
-for model in models:
-    print(model.name)
