8aafe9c1a4f2a8cd6d0dd4dd624fd34247bacf46
Rudra1808 <146955248+dubeyrudra-1808@users.noreply.github.com>
2025-07-18 18:07:13 +0530

    for backend

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
-#!/usr/bin/env bash
-# Backend
-python -m venv venv && source venv/bin/activate
-pip install -r requirements.txt &
-# Frontend
-cd Frontend/frontend && npm install &
-wait
+#!/bin/bash
+pip install -r requirements.txt
+uvicorn app.main:app --host=0.0.0.0 --port=10000
